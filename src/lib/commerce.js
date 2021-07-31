b171deed52fde5db4a7821f2c47261a5762f1efc
freakbeast14 <shahilmohammed14@gmail.com>
2021-07-31 15:43:23 +0530

    week-1

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-// export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
-export const commerce = new Commerce("pk_184625ed86f36703d7d233bcf6d519a4f9398f20048ec", true);
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
